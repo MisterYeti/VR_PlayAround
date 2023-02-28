d3515ee5f5122b3c4ad49db0bd40eca205b7d61d
Samoth <tbiffaud@gmail.com>
2023-02-28 15:12:13 +0100

    Distance Grab

---
+++ Assets/_Scripts/ActivateGrabRay.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ActivateGrabRay : MonoBehaviour
+{
+    [SerializeField] private GameObject _leftGrabRay;
+    [SerializeField] private GameObject _rightGrabRay;
+
+    [SerializeField] private XRDirectInteractor _leftDirectGrab;
+    [SerializeField] private XRDirectInteractor _rightDirectGrab;
+
+    private void Update()
+    {
+        _leftGrabRay.SetActive(_leftDirectGrab.interactablesSelected.Count == 0);
+        _rightGrabRay.SetActive(_rightDirectGrab.interactablesSelected.Count == 0);
+    }
+}
---
+++ Assets/_Scripts/XRGrabInteractableTwoAttach.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrabInteractableTwoAttach : XRGrabInteractable
+{
+    [SerializeField] private Transform leftAttachTransform; 
+    [SerializeField] private Transform rightAttachTransform;
+
+    protected override void OnSelectEntering(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject.transform.CompareTag("Left Hand"))
+        {
+            attachTransform = leftAttachTransform;
+        }
+        else if (args.interactorObject.transform.CompareTag("Right Hand"))
+        {
+            attachTransform = rightAttachTransform;
+        }
+        base.OnSelectEntering(args);
+    }
+}
